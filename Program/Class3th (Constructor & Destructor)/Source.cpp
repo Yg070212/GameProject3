b2d3b4595d11680bbbeb4c7dfc3e42b011beb1f9
Yg070212 <winter070212@gmail.com>
2025-01-15 19:43:35 +0900

    2025-01-15 <생성자와 소멸자>

---
+++ Program/Class3th (Constructor & Destructor)/Source.cpp
@@ -0,0 +1,90 @@
+﻿#include <iostream>
+
+using namespace std;
+
+namespace Austraila
+{
+	int area = 36687;
+
+	void size()
+	{
+		cout << "Austraila Area : " << area << endl;
+	}
+}
+
+namespace China
+{
+	int area = 987663;
+
+	void size()
+	{
+		cout << "China Area : " << area << endl;
+	}
+}
+
+using namespace Austraila;
+
+class Object
+{
+private:
+	static int count;
+
+public:
+	Object()
+	{
+		cout << "Created object" << endl;
+	}
+
+	~Object()
+	{
+		count++;
+
+		cout << "Kill Count : " << count << endl;
+
+		cout << "Destroy Object" << endl;
+	}
+};
+
+int Object::count = 0;
+
+int main()
+{
+#pragma region 이름 공간
+	// 속성을 구분할 수 있도록 유효 범위를 설정하는 영역입니다.
+
+	// size();
+	// China::size();
+
+#pragma endregion
+
+#pragma region 생성자
+	// 클래스의 인스턴트가 생성되는 시점에서 자동으로
+	// 호출되는 특수한 멤버 함수 입니다.
+
+	//Object object;
+
+	// 생성자의 경우 객체가 생성될 때 단 한 번만 호출되며,
+	// 생성자는 반환형이 존재하지 않기 때문에 생성자가 호출되기
+	// 전에는 객체에 대한 메모리가 할당되지 않습니다.
+#pragma endregion
+
+#pragma region 소멸자
+	// 객체가 소멸될 떄 자동으로 실행되는 클래스의
+	// 멤버 함수 입니다.
+
+	// Object * object1 = new Object;
+	// Object * object2 = new Object;
+	// Object * object3 = new Object;
+	// 
+	// delete object1;
+	// delete object2;
+	// delete object3;
+	// 
+	// 소멸자는 객체가 메모리에서 해제될 때 단 한 번만
+	// 호출되며, 소멸자에는 매개 변수를 생성하여 사용할
+	// 수 없습니다.
+#pragma endregion
+
+
+	return 0;
+}
--- Program/Program/Program.cpp
+++ Program/Program/Program.cpp
@@ -1,79 +1,9 @@
 ﻿#include <iostream>
 
-class GameObject
-{
-#pragma region 접근 지정자
-	// 클래스 내부에 포함되어 있는 속성에 접근 범위를
-	// 제한하는 지정자 입니다.
-
-	// public : 클래스 내부와 자기가 상속하고 있는 클래스 그리고 클래스
-	//          외부에서도 접근을 허용하는 지정자 입니다.
-
-	// protected : 클래스 내부와 자기가 상속하고 있는 클래스 까지만
-	//             접근을 허용하는 지정자 입니다.
-
-	// private : 클래스 내부까지만 접근을 허용하는 지정자 입니다.
-
-#pragma endregion
-
-private:
-	int x;
-protected:
-	int y;
-public:
-	int z;
-
-	int  Z()
-	{
-		return z;
-	}
-};
-
-void Swap(int & x, int & y)
-{
-	int temporary = x;
-	x = y;
-	y = temporary;
-}
+using namespace std;
 
 int main()
 {
-#pragma region 클래스
-	// 사용자 정의 데이터 유형으로 속성과 함수가 포함
-	// 되어 있으며, 클래스를 통해 객체를 생성하여 접근
-	// 하고 사용하는 집합체 입니다.
-
-	GameObject gameObject1; // [ ] -> Stack
-	GameObject gameObject2;
-	GameObject gameObject3;
-
-	gameObject1.z = 5;
-	gameObject2.z = 10;
-	gameObject3.z = 20;
-
-	std::cout << "gameObject1의 z 값 : " << gameObject1.Z() << std::endl;
-	std::cout << "gameObject2의 z 값 : " << gameObject2.Z() << std::endl;
-	std::cout << "gameObject3의 z 값 : " << gameObject3.Z() << std::endl;
-
-
-	// 클래스의 경우 클래스 내부에 있는 변수는 클래스의
-	// 메모리 영역에 포함되지만, 정적 변수와 함수의 메모리는
-	// 클래스 영역에 포함되지 않습니다.
-#pragma endregion
-
-#pragma region 참조자
-	// 어떤 변수의 메모리 공간에 다른 이름을
-	// 지정하는 지정자 입니다.
-
-	int a = 10;
-	int b = 20;
-
-	Swap(a, b);
-
-	std::cout << "a의 값 : " << a << std::endl;
-	std::cout << "b의 값 : " << b << std::endl;
-
-#pragma endregion
 
 
 	return 0;
