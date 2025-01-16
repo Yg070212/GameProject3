53c671002e59cd4912f40885a94ca2ff9b2d8bf3
Yg070212 <winter070212@gmail.com>
2025-01-16 19:47:05 +0900

    2025-01-16 <f복사 생성자>

--- Program/Program/Program.cpp
+++ Program/Program/Program.cpp
@@ -2,9 +2,75 @@
 
 using namespace std;
 
+class Unit
+{
+private:
+	int health = 0;
+
+	int* kill;
+
+public:
+	Unit(int health)
+	{
+#pragma region this 포인터
+		// 자기 자신을 가리키는 포인터 입니다.
+
+#pragma endregion
+
+		this->health = health;
+
+		kill = new int;
+
+		*kill = 0;
+	}
+
+	Unit(Unit & clone)
+	{
+		health = clone.health;
+
+		kill = clone.kill;
+	}
+
+	const int & Health()
+	{
+		return health;
+	}
+
+	~Unit()
+	{
+		delete kill;
+	}
+};
+
 int main()
 {
+#pragma region 복사 생성자
+	// 같은 객체를 복사하여 생성시킬 때 호출되는
+	// 생성자 입니다.
 
+	Unit marine1(40);
+	Unit marine2(marine1);
+
+	cout << marine1.Health() << endl;
+	cout << marine2.Health() << endl;
+
+	// 복사 생성자를 정의하지 않고, 객체를 복사하게 되면
+	// 기본 복사 생성자가 호출되기 때문에 얕은 복사로 호출됩니다.
+
+#pragma endregion
+
+#pragma region 얕은 복사
+	// 객체를 복사 할 때 주소 값을 복사하여 같은
+	// 메모리 공간을 가리키게 하는 것 입니다.
+
+	Unit firebet1(50);
+
+	Unit firebet2(firebet1);
+
+	// 얕은 복사의 경우 같은 객체가 서로 같은
+	// 공간을 참조하고 있기 때문에 하나의 객체 값을
+	// 변경하게 되면 서로 참조된 객체도 함께 영향을 받습니다.
+#pragma endregion
 
 	return 0;
 }
