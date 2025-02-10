cf9ffec0b4b9a14289b92f2c74562b3e5369b6be
Yg070212 <winter070212@gmail.com>
2025-02-10 19:44:45 +0900

    2025-02-10 <연관 컨테이너>

---
+++ Program/Class14th (Container Adapter)/Source.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+#pragma region Stack
+
+    // stack<int> stack;
+    // 
+    // stack.push(30);
+    // stack.push(20);
+    // stack.push(10);
+    // 
+    // cout << "Stack Size : " << stack.size() << endl;
+    // 
+    // while (stack.empty() == false)
+    // {
+    //     cout << stack.top() << endl;
+    // 
+    //     stack.pop();
+    // }
+
+#pragma endregion
+
+#pragma region Queue
+    // queue<int> queue;
+    // 
+    // queue.push(10);
+    // queue.push(20);
+    // queue.push(30);
+    // queue.push(40);
+    // queue.push(50);
+    // 
+    // while (queue.empty() == false)
+    // {
+    //     cout << queue.front() << " ";
+    // 
+    //     queue.pop();
+    // }
+
+#pragma endregion
+
+    return 0;
+}
--- Program/Program/Program.cpp
+++ Program/Program/Program.cpp
@@ -1,29 +1,10 @@
 ﻿#include <iostream>
-#include <stack>
+
 
 using namespace std;
 
 int main()
 {
-#pragma region Stack
-
-    stack<int> stack;
-
-    stack.push(30);
-    stack.push(20);
-    stack.push(10);
-
-    cout << "Stack Size : " << stack.size() << endl;
-
-    while (stack.empty() == false)
-    {
-        cout << stack.top() << endl;
-
-        stack.pop();
-    }
-
-#pragma endregion
-
 
 
     return 0;
